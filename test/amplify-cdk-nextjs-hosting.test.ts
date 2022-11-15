0b1c78aba6a1e65a603842dd666393ccea59bf9d
Michael Liendo <mtliendo@gmail.com>
2022-11-15 08:45:09 -0600

    Initial commit

---
+++ test/amplify-cdk-nextjs-hosting.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AmplifyCdkNextjsHosting from '../lib/amplify-cdk-nextjs-hosting-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/amplify-cdk-nextjs-hosting-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AmplifyCdkNextjsHosting.AmplifyCdkNextjsHostingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
